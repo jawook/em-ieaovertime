072be03e11cbef2ec1c9fdf0686243623a599700
jawook <95268438+jawook@users.noreply.github.com>
2024-02-12 23:09:26 -0700

    Update ieaovertime.py

--- ieaovertime.py
+++ ieaovertime.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+# dictionary to map data
 mapper = {'Coal': {'hist': ['coalcons_ej'], 
                     'fcst': ['Coal']},
           'Oil': {'hist': ['oilcons_ej'],
@@ -13,18 +14,42 @@ mapper = {'Coal': {'hist': ['coalcons_ej'],
           'Bioenergy': {'hist': ['biofuels_cons_ej', 'biogeo_ej'],
                         'fcst': ['Bioenergy']},
           'Hydro': {'hist': ['hydro_ej'],
-                    'fcst': ['Hydro']}}
+                    'fcst': ['Hydro']},
+          'Other Renewables': {'hist': 'renewables_ej',
+                               'fcst': 'Other renewables'},
+          'Total': {'hist': ['primary_ej'],
+                    'fcst': ['Coal', 'Oil', 'Natural Gas',
+                             'Nuclear', 'Bioenergy', 'Hydro']}}
 
+# load and pre-process data
 fcsts = pd.read_excel('ConsolSources.xlsx', 'Consolidated')
 hists = pd.read_csv('SRofWE.csv')
 allHistSer = []
 for j in mapper:
     allHistSer.extend(mapper[j]['hist'])
 hists = hists[(hists['Country']=='Total World') & 
-              (hists['Year'] >= 2000) & 
+              (hists['Year'] >= 2010) & 
               (hists['Var'].isin(allHistSer))]
+
+#make a list of energy sources
+enList = list(mapper.keys())
+
+st.write("TEST")
+en = st.selectbox(label="Choose an energy source to evaluate", 
+                  options=enList)
+
+#make a year range
+yMin = min(min(fcsts['fcstYear']), min(hists['Year']))
+yMax = max(max(fcsts['fcstYear']), max(hists['Year']))
+
+#make a forecast range
+fcstYMin = min(fcsts['RPT'])
+fcstYMax = max(fcsts['RPT'])
+
+
+
 
 plt = px.line(hists, x='Year', y='Value', color='Var', animation_frame='Year')
 
-st.write("TEST")
+
 st.plotly_chart(plt)
