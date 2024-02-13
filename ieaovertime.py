ed3b010e8f37c9788dc0d4b0c75ef35c9c18a061
jawook <95268438+jawook@users.noreply.github.com>
2024-02-12 20:19:37 -0700

    Update ieaovertime.py

--- ieaovertime.py
+++ ieaovertime.py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 fcsts = pd.read_excel('ConsolSources.xlsx', 'Consolidated')
+hists = pd.read_excel('ConsolSources.xlsx', 'History')
 
 date = fcsts[(fcsts['Source']=='Coal') & (fcsts['scen']=='Stated Policies Scenario')]
 plt = px.line(date, x='fcstYear', y='Value', color='RPT')
