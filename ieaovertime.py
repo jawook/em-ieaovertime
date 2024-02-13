afe3192d90ee8eb7d5af63dce5ca3e2c89168534
jawook <95268438+jawook@users.noreply.github.com>
2024-02-12 20:18:03 -0700

    Update ieaovertime.py

--- ieaovertime.py
+++ ieaovertime.py
@@ -1,14 +1,11 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import plotly.io as pio
-pio.renderers.default = 'browser'
 
 fcsts = pd.read_excel('ConsolSources.xlsx', 'Consolidated')
 
 date = fcsts[(fcsts['Source']=='Coal') & (fcsts['scen']=='Stated Policies Scenario')]
 plt = px.line(date, x='fcstYear', y='Value', color='RPT')
-plt.show()
 
 st.write("TEST")
 st.plotly_chart(plt)
